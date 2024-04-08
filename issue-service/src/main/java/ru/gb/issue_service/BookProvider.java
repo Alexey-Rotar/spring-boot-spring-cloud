d1dabc67b6e17df375d7654091f2038e27743dd0
alexey.rotar <ar_mail@inbox.ru>
2024-04-08 14:49:04 +0500

    initial

---
+++ issue-service/src/main/java/ru/gb/issue_service/BookProvider.java
@@ -0,0 +1,50 @@
+package ru.gb.issue_service;
+
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
+import com.netflix.discovery.shared.Application;
+import org.springframework.cloud.client.loadbalancer.reactive.ReactorLoadBalancerExchangeFilterFunction;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.List;
+import java.util.Random;
+import java.util.UUID;
+
+@Component
+// класс, обеспечивающий подключение к ресурсу book-service (провайдер ресурса book-service)
+public class BookProvider {
+
+    private final WebClient webClient;
+//    private final EurekaClient eurekaClient;
+
+    public BookProvider(EurekaClient eurekaClient, ReactorLoadBalancerExchangeFilterFunction loadBalancerExchangeFilterFunction) {
+        webClient = WebClient.builder()
+                .filter(loadBalancerExchangeFilterFunction)
+                .build();
+//        this.eurekaClient = eurekaClient; // нужен при
+    }
+
+    public UUID getRandomBookId() {
+        Book randomBook = webClient.get()
+//                .uri("http://localhost:8580/book/random") // запрос напрямую к сервису, без Eureka!
+//                .uri(getBookServiceIp() + "/book/random")
+                .uri("http://book-service/book/random")
+                .retrieve()
+                .bodyToMono(Book.class)
+                .block();
+        return randomBook.getId();
+    }
+
+//    private String getBookServiceIp(){
+//        Application application = eurekaClient.getApplication("BOOK-SERVICE");
+            // их может быть несколько с таким именем
+            // т. е. может быть несколько серверов, выполняющих одну функцию, если большая нагрузка
+//        List<InstanceInfo> instanceInfos = application.getInstances(); // поэтому лист
+
+//        Random random = new Random();
+            // берем любой в пределах списка
+//        InstanceInfo randomInstance = instanceInfos.get(random.nextInt(instanceInfos.size()));
+//        return "http://" + randomInstance.getIPAddr() + ":" + randomInstance.getPort();
+//    }
+}
